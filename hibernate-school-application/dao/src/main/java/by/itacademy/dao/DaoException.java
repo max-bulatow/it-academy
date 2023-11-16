3fd25b6169c861c5920fa252d97725dba1177221
max-bulatow <143122843+max-bulatow@users.noreply.github.com>
2023-11-16 23:07:51 +0300

    Delete hibernate-school-application/dao/src/main/java/by/itacademy/dao/DaoException.java

--- hibernate-school-application/dao/src/main/java/by/itacademy/dao/DaoException.java
+++
@@ -1,7 +0,0 @@
-package by.itacademy.dao;
-
-public class DaoException extends Exception {
-    public DaoException(final String message, final Throwable cause) {
-        super(message, cause);
-    }
-}
