77b0f1e20f6f4f66c7f86b9f7e73f87f5592367f
max-bulatow <max.bulatow@gmail.com>
2024-01-09 16:22:14 +0300

    spring-web-application

---
+++ spring-web-application/src/main/java/by/itacademy/web/ApplicationInitializer.java
@@ -0,0 +1,24 @@
+package by.itacademy.web;
+
+import by.itacademy.web.config.ContextConfiguration;
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(final ServletContext servletContext) throws ServletException {
+        final var webContext = new AnnotationConfigWebApplicationContext();
+        webContext.register(ContextConfiguration.class);
+        webContext.setServletContext(servletContext);
+
+        final var dispatcherServlet = new DispatcherServlet(webContext);
+        final var servlet = servletContext.addServlet("dispatcher", dispatcherServlet);
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+
+}
---
+++ spring-web-application/src/main/java/by/itacademy/web/config/ContextConfiguration.java
@@ -0,0 +1,9 @@
+package by.itacademy.web.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@ComponentScan(basePackages = "by.itacademy.web")
+@Configuration
+public class ContextConfiguration {
+}
